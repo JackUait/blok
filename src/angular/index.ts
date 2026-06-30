df46ff4bc7441f1327abad7197d4c512fc284870
JackUait <jackuait@gmail.com>
2026-06-30 21:05:21 +0300

    feat(angular): export createAngularBlock, injectBlocks, BLOK_BLOCK_CONTEXT

--- src/angular/index.ts
+++ src/angular/index.ts
@@ -1,4 +1,19 @@
 export { BlokEditorComponent } from './blok-editor.component';
 export { BlokContentDirective } from './blok-content.directive';
 export { provideBlok, BLOK_DEFAULT_CONFIG } from './provide-blok';
+export { createAngularBlock } from './createAngularBlock';
+export { injectBlocks } from './useBlocks';
+export { BLOK_BLOCK_CONTEXT } from './block-context';
 export type { BlokAngularConfig } from './types';
+export type { AngularBlockRenderContext } from './block-context';
+export type { CreateAngularBlockSpec } from './createAngularBlock';
+export type { PropSchema, PropSchemaEntry } from '../shared/prop-schema';
+export type {
+  UseBlocksApi,
+  BlockNode,
+  CaretTarget,
+  InsertPosition,
+  InsertSpec,
+  TreeInsertSpec,
+  MoveTarget,
+} from './blocks-snapshot';
